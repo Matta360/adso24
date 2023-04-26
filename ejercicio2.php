8e9a3bb5b5df27f7044828a06ed6509be2971fcc
Matta360 <131733081+Matta360@users.noreply.github.com>
2023-04-26 16:18:58 -0500

    Delete ejercicio2.php

--- ejercicio2.php
+++
@@ -1,8 +0,0 @@
-<?php
-//2.Solicitar al usuario un número y calcular cuál es el cuadrado del número ($2^2=4$).
-$numero1 = 10;
-$elevar = $numero1 * $numero1;
-
-echo "resultado:", $elevar;
-echo ("<br>");
-?>
