835abf3a5b34213d2c285da1ab92a1b7cf9ce595
Matta360 <131733081+Matta360@users.noreply.github.com>
2023-04-26 16:19:29 -0500

    Delete ejercicio7.php

--- ejercicio7.php
+++
@@ -1,8 +0,0 @@
-<?php
-//7.Programa para calcular el área de un cuadrado, la longitud de un lado la ingresa el usuario.
-$numero1 = 6;
-$multiplicacion = $numero1 * 4;
-
-echo "el area del cuadrado es:", $multiplicacion;
-echo ("<br>");
-?>
