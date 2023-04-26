0b5ea63e659e0dbe4b31b977f25cca3b05159345
Matta360 <131733081+Matta360@users.noreply.github.com>
2023-04-26 16:19:11 -0500

    Delete ejercicio4.php

--- ejercicio4.php
+++
@@ -1,9 +0,0 @@
-<?php
-//4.Programa que permita calcular la edad de una persona conociendo el año actual y el usuario digita su año de nacimiento.
-$año = 2023;
-$fecha = 2003;
-$edad = $año - $fecha;
-
-echo "su edad es:", $edad, "años";
-echo ("<br>");
-?>
